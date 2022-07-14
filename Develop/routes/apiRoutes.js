5b53d55733142426915cee2e2a8863f5f27bad0e
GSvenny <gabrielswagner7@gmail.com>
2022-07-14 01:04:31 -0500

    some progress with server

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const util = require("util");
+const app = require("express").Router();
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+
+
+app.get("/notes", (req, res) => {
+  // Reads the notes from JSON file
+  readFileAsync("db/db.json", "utf8").then(function (data) {
+    // Parse data to get an array of objects
+    notesData = JSON.parse(data);
+    //
+    res.json(notesData);
+  });
+});
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App currently on PORT ${PORT}`)
 });
+
+module.exports = app;
